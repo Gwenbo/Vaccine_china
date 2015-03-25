6b7f70836dde7d649e75107171fab07fee707808
rebeccaclaireharris <rebecca.harris@lshtm.ac.uk>
2015-03-25 14:30:17 +0000

    graph to compare to Gao

--- TBPI by age plot.R
+++ TBPI by age plot.R
@@ -4,7 +4,26 @@ agegrps=c("5-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70+")
 
 tTBPI<-t(TBPI)
 
-read.
+setwd("/Users/Rebecca/Vaccine_china/Data")
+gaodata<-suppressWarnings(as.matrix(read.csv('gaoLTBI.csv',header=TRUE,check.names=F, row.names=1)))
+gaodata<-t(gaodata)
+gaomodel<-cbind(gaodata,tTBPI[7:14,114])
+colnames(gaomodel, do.NULL=FALSE, prefix="col")
+colnames(gaomodel)<-c("QFN", "TST", "Model")
+rownames(gaomodel)<-c("5-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70+")
+gaomodel
+
+### comparison to Gao
+par(mfrow=c(1,1))
+ages<-c("5-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70+")
+plot(seq(1,8), gaomodel[,1], xaxt = "n", main="China 2013 LTBI estimates: Comparison of data and model", xlab="Age group(years)", ylab="LTBI Prevalence (%)",ylim=c(0,80),type='l', col=4)
+axis(1, at=1:8, labels=ages)
+points(seq(1,8), gaomodel[,2], type='l', col=3)
+points(seq(1,8), gaomodel[,3], type='l', col=2)
+legend("topleft", c("Model", "TST", "QFN"), lty=1,col=c(2,3,4), border="white")
+
+
+
 
 
 ##plots of TBPI by age and every 10 years
@@ -21,4 +40,7 @@ plot(seq(1,8),tTBPI[7:14,131], ylab="Prevalence of Infection 2030",xlab="Age gro
 plot(seq(1,8),tTBPI[7:14,141], ylab="Prevalence of Infection 2040",xlab="Age group(years)", ylim=c(0,100),type='l',col='blue')
 
 plot(seq(1,8),tTBPI[7:14,151], ylab="Prevalence of Infection 2050",xlab="Age group(years)", ylim=c(0,100),type='l',col='purple')
+
+
+
 
