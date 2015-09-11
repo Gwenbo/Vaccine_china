7ebd92e6e8581c3a34a383e4ae0732f47e50c6e8
rebeccaclaireharris <rebecca.harris@lshtm.ac.uk>
2015-09-11 11:00:28 +0100

    sh for appending on cluster - not working at the moment

--- appendxout.sh
+++ appendxout.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
 
-#  appendxout.sh
-#  
-#
-#  Created by Rebecca Harris on 11/09/2015.
-#
+#!/bin/bash
+#$ -N chinaappend
+#$ -cwd -V
+#$ -q short.q
+#$ -l mem_free=6G,h_vmem=6.2G
+#$ -M rebecca.harris@lshtm.ac.uk -m eas
+#$ -t 1-1
+
+R CMD BATCH /home/lsh355020/China/clusterappend.R appendxout_china${SGE_TASK_ID}.out
+
