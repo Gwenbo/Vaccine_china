8b95e34e995823b4b5c66311ab57f8f0b3c8a8dc
rebeccaclaireharris <rebecca.harris@lshtm.ac.uk>
2015-10-14 16:13:07 +0100

    fix due to adding TBRa/i to xout

--- cluster_china.R
+++ cluster_china.R
@@ -74,7 +74,7 @@ tail(randparam)
 
 #enter value for P0
 randparam[1:n_p,(length(nm)+1)]<-rep(1267142,n_p) ##insert p0 value here (1267142 from manual model fit???)##
-head(randparam)
+
 #randparam needs to be saved as paraout_China.csv in data file
 setwd(home);setwd("Data")
 write.table(randparam,paste("paraout_",cntry,"_",job,".csv",sep=''),sep=",",row.names=FALSE) #sep within paste tells how the elements should be separated (baseline is to assume space), and in write.table sep is to say how the data are separated
@@ -86,8 +86,6 @@ setwd(home);setwd("Data")
 para<-read.csv(paste("paraout_",cntry,"_",job,".csv",sep=''))[-1]
 setwd(home)
 
-head(para)
-tail(para)
 #timestep, start and end year
 dt<-(1/2)
 year1<-1900
@@ -99,7 +97,7 @@ year<-c(seq(year1,yearend,1),rep(0,((1/dt)*(yearend-year1+1)-(yearend-year1+1)))
 count<-1 #remove once doing vacc scenarios
 nn<-1 #remove once doing vacc scenarios
 nmbr<-count*nn #use this once doing vaccine scenarios
-xout<-mat.or.vec(((yearend-year1+1)*(1/dt)*n_p*(typen+1)),99) #(94+run number+vacc type+vacc effic) would need changing if extra outputs added
+xout<-mat.or.vec(((yearend-year1+1)*(1/dt)*n_p*(typen+1)),109) #(94+run number+vacc type+vacc effic+10*TBRa/i) would need changing if extra outputs added
 
 
 # Simulate data 
@@ -122,7 +120,6 @@ xout<-as.data.frame(xout)
 xout<-cbind(xout,JOB)
 colnames(xout)<-c(colnames(Xn),"timestep","year","type","vxint","fit","job") #timestep is for params where given by timestep, year is for where output is summary of annual
     
-
 head(xout)
 tail(xout)
 #write results to csv file on the cluster
