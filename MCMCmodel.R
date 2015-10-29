0206e15d5a66868c201d5c629aaa76ecea21c78c
rebeccaclaireharris <rebecca.harris@lshtm.ac.uk>
2015-10-29 13:42:32 +0000

    Check that elderly params are bigger/smaller than adult, and reject immediately if not

--- MCMCmodel.R
+++ MCMCmodel.R
@@ -3,9 +3,17 @@
 MCMCmodel<-function(para){
   
   #para<-para
-  #para<-inipa
+  para<-inipa
   para<-as.data.frame(para)
   #call in data sets and Cfunction
+  
+  #### check that elderly/infant parameters are bigger/smaller than adult (as appropriate)
+  
+  if ((para[9,1]>=para[10,1]) & (para[11,1]>=para[21,1]) & (para[12,1]>=para[23,1]) & (para[13,1]>=para[14,1]) & (para[16,1]>=para[17,1]) & (para[18,1]<=para[22,1]) & (para[19,1]>=para[26,1]) & (para[23,1]<=para[24,1])) {paracheck<-1} else {paracheck<-0}
+  print(paracheck)
+  
+  if (paracheck==0){AR <- c(0,0)} else {
+  
   C=0
   if (C == 0){home<-"/Users/Rebecca/Vaccine_china"}
   if (C == 1){home<-"/home/lsh355020/China/"}
@@ -21,6 +29,8 @@ MCMCmodel<-function(para){
   nm<-c(pararange[,1],"p0") # The parameter ranges
   dt<-0.5  #timestep
   
+  
+  
   #run the model
   for (i in 1:(length(nm)+1)){assign(nm[i],as.numeric(para[i,]))} # Assign the parameters to the correct values
   neta2<-neta # this parameter needs extra assigning for some annoying reason! 
@@ -35,11 +45,13 @@ MCMCmodel<-function(para){
   #compare model to data ranges and calc number of hits (fithit)
   source("fithit.R")
   print(fithit)
-  ###### Aceept or reject? Adjust fit hit threshold as required
+  ###### Aceept or reject? Adjust fit hit threshold as required. Keep = 1
   AR <- c(0,0)
   
   if(fithit >=8) AR <- c(1,0) 
   
+  #end of ifelse for if parameters are right sizes
+  }
   
   #return summary statistic of the number of data points the model is hittting
   return(AR)
